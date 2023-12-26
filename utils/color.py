e5045319935bac4d6cadcc72d6b73cb162fd8477
Stcnk <146685317+Stcnk@users.noreply.github.com>
2023-12-26 14:37:43 +0700

    get system info data

--- utils/color.py
+++ utils/color.py
@@ -1,7 +1,42 @@
 
 from reportlab.lib.pagesizes import A4
 
+## ### total server
+
+blue = '#50ace7'
+
 main_blue = '#4FACE6'
+
+
+## performance
+
+red = '#ff3131'
+
+yellow = '#ff3131'
+
+green = '#7ed957'
+
+### graph zabbix
+
+pink_dot = '#fc66cc'
+
+purple_dot = '#745cac'
+
+green_dot = '#64d4d4'
+
+## donut chart zabbix
+
+dark_green ='#6aa84f'
+
+soft_green ='#b7d7a8'
+
+dark_yellow= '#f7c927'
+
+soft_yellow ='#fdf2cc'
+
+dark_red = '#cc1a01'
+
+soft_red=' #f5cccc'
 
 
 def set_background_color(c,color=main_blue):
